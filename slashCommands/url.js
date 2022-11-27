8950729ffc5021e7c4afbcf5ebe9048ecf872b96
Tobydog0501 <tobydog0501@gmail.com>
2022-11-27 11:51:36 +0800

    invite link

---
+++ slashCommands/url.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  data:new SlashCommandBuilder()
+	      .setName('url')
+	      .setDescription('創建邀請')
+      	,
+  async execute(inter,Discord){
+    if(!inter.member.roles.cache.some(v=>v.id=="1044264943874224228")||!inter.member.roles.cache.some(v=>v.id=="1044264999100612638")){
+        await inter.reply({content:"你不能這樣做",ephemeral:true});
+        return;
+    }
+    const invite = await inter.guild.invites.create("1044265231842545756",{maxAge:0,maxUses:1})
+    const ebd = new EmbedBuilder()
+        .setTitle("邀請連結生成成功")
+        .setDescription(`invite link: ${invite} `)
+        .setFooter("此連結僅能使用一次")
+
+
+    await inter.reply({embeds:[ebd],ephemeral:true});
+    
+  }
+}
